cb6ab1b3682db43700130faedbc4599f0b5a2cc3
Damien <Damien@DAMIENROSA>
2017-08-18 01:44:02 +0200

    Add project files.

---
+++ InsuranceApolicyWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InsuranceApolicyWeb.Startup))]
+namespace InsuranceApolicyWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
