2ab8fd6f25f27e53245ccdeca00fc457a6587bb2
E552 <p5YoCZzT>
2017-08-22 19:43:12 +0100

    * updated personal details

--- InsuranceApolicyWeb/Models/UserViewModel.cs
+++ InsuranceApolicyWeb/Models/UserViewModel.cs
@@ -6,11 +6,23 @@ using System.Web;
 
 namespace InsuranceApolicyWeb.Models
 {
-    public enum DocumentType { BI, CC, PASSPORT }
+    public enum DocumentType { BI, CC,
+        [Display(Name = "Passaport")]
+        PASSPORT }
 
-    public enum ClientType { P, E }
+    public enum ClientType {
+        [Display(Name = "Particulares")]
+        P,
+        [Display(Name = "Empresas")]
+        E }
 
-    public enum RoleType { Mr, Ms, Mrs }
+    public enum RoleType {
+        [Display(Name = "Sr.")]
+        Mr,
+        [Display(Name = "Sra.")]
+        Ms,
+        [Display(Name = "Sra.")]
+        Mrs }
 
     public class UserViewModel
     {
@@ -18,30 +30,43 @@ namespace InsuranceApolicyWeb.Models
 
         public string NIF { get; set; }
 
+        [Display(Name = "Título")]
         public RoleType Role { get; set; }
 
+        [Display(Name = "Tipo Cliente")]
         public ClientType ClientType { get; set; }
 
+        [Display(Name = "Nome")]
         public string Name { get; set; }
 
+        [Display(Name = "Tipo Documento")]
         public DocumentType DocumentType { get; set; }
 
+        [Display(Name = "Nº Documento")]
         public string DocumentNumber { get; set; }
 
+        [Display(Name = "Rua")]
         public string AddressStreet { get; set; }
 
+        [Display(Name = "Nº")]
         public int AddressNumber { get; set; }
 
+        [Display(Name = "Bairro")]
         public string AddressNeighborhood { get; set; }
 
+        [Display(Name = "Município")]
         public string AddressTown { get; set; }
 
+        [Display(Name = "Cidade")]
         public string AddressCity { get; set; }
 
+        [Display(Name = "Província")]
         public string AddressDistrict { get; set; }
 
+        [Display(Name = "Telefone")]
         public string PhoneNumberHome { get; set; }
 
+        [Display(Name = "Telemóvel")]
         public string PhoneNumberPersonal { get; set; }
 
         public string Email { get; set; }
